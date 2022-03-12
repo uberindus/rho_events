1bb3fe043388604993259113820bcdc679fac4c0
eldarguseynli <eldarguseynli@yandex-team.ru>
2022-03-12 16:43:32 +0300

    fix

--- backend/entrypoint.dev.sh
+++ backend/entrypoint.dev.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+# if [ "$DATABASE" = "postgres" ]
+# then
+#     echo "Waiting for postgres..."
 
-    while ! nc -z $DB_HOST $DB_PORT; do
-      sleep 0.1
-    done
+#     while ! nc -z $DB_HOST $DB_PORT; do
+#       sleep 0.1
+#     done
 
-    echo "PostgreSQL started"
-fi
+#     echo "PostgreSQL started"
+# fi
 
 python manage.py flush --no-input
 python manage.py makemigrations
